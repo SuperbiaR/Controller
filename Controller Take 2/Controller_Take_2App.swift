26fbca75a9e44165b17f059c3b355f11b3419510
SuperbiaR <altierstuff@gmail.com>
2021-10-21 12:18:52 -0500

    Initial Commit

---
+++ Controller Take 2/Controller_Take_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Controller_Take_2App.swift
+//  Controller Take 2
+//
+//  Created by PMStudent on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Controller_Take_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
